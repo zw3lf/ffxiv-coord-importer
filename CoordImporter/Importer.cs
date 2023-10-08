a789e5499e6f15c92e96ed2919bd41ccead82643
zw3lf <zw3lf@github.com>
2023-10-08 17:25:35 +0100

    Attempt at getting NUnit working for the regexes

---
+++ CoordImporter/Importer.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Dalamud.Game.Text;
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Game.Text.SeStringHandling.Payloads;
+using Dalamud.Plugin.Services;
+using Dalamud.Utility;
+using Lumina.Excel.GeneratedSheets;
+
+namespace CoordImporter
+{
+    public class MarkInformation
+    {
+        public MapData? map { get; }
+        public float xCoord { get; }
+        public float yCoord { get; }
+        public string? instanceId { get; }
+        public string markName { get; }
+        public string inputLine { get; }
+        public string mapName { get; }
+
+        public MarkInformation(MapData? map, float xCoord, float yCoord, string? instanceId, string markName, string inputLine, string mapName)
+        {
+            this.map = map;
+            this.xCoord = xCoord;
+            this.yCoord = yCoord;
+            this.instanceId = instanceId;
+            this.markName = markName;
+            this.inputLine = inputLine;
+            this.mapName = mapName;
+        }
+    }
+
+    public class Importer
+    {
+        private IChatGui Chat { get; }
+        private IPluginLog Logger { get; }
+        private IDictionary<String, MapData> Maps { get; }
+
+        public Importer(
+            IChatGui chat,
+            IPluginLog logger,
+            IDictionary<String, MapData> maps)
+        {
+            Chat = chat;
+            Logger = logger;
+            Maps = maps;
+        }
+
+        public List<MarkInformation> ParsePayload(string pastedPayload)
+        {
+            var instanceKeyMap = new Dictionary<String, String>()
+            {
+                { "1", "\ue0b1" },
+                { "2", "\ue0b2" },
+                { "3", "\ue0b3" },
+            };
+
+            var splitStrings = pastedPayload.Split(
+                new string[] { "\r\n", "\r", "\n" },
+                StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            // For the format "(Maybe: Storsie) \ue0bbLabyrinthos ( 17  , 9.6 ) " (including the icky unicode instance/arrow)
+            var sirenRegex = new Regex(
+                @"(\(Maybe: (?<mark_name>[\w+ '-]+)\) \ue0bb)?(?<map_name>[\w+ '-]+)(?<instance_id>[\ue0b1\ue0b2\ue0b3]?)\s+\(\s*(?<x_coord>[0-9\.]+)\s*,\s*(?<y_coord>[0-9\.]+)\s*\)",
+                RegexOptions.Compiled);
+            // For the format "Labyrinthos ( 16.5 , 16.8 ) Storsie"
+            var bearRegex = new Regex(
+                @"(?<map_name>[\D'\s+-]*)\s*(?<instance_number>[123]?)\s*\(\s*(?<x_coord>[0-9\.]+)\s*,\s*(?<y_coord>[0-9\.]+)\s*\)\s*(?<mark_name>[\w+ +-]+)",
+                RegexOptions.Compiled);
+            // For the format "Raiden [S]: Gamma - Yanxia ( 23.6, 11.4 )"
+            var faloopRegex = new Regex(
+                @"(?<world_name>[a-zA-Z0-9'-]+)\s+\[S\]: (?<mark_name>[\w+ '-]+) - (?<map_name>[\w+ '-]+)\s+\(?(?<instance_number>[1-3]?)\)?\s*\(\s*(?<x_coord>[0-9\.]+)\s*,\s*(?<y_coord>[0-9\.]+)\s*\)",
+                RegexOptions.Compiled);
+
+            List<MarkInformation> marks = new List<MarkInformation>();
+            foreach (var inputLine in splitStrings)
+            {
+                Match match;
+                string? instanceId;
+                GroupCollection groups;
+                // Check if the little arrow symbol is in the text. If it is then the line is from Siren
+                if (inputLine.Contains("\ue0bb"))
+                {
+                    match = sirenRegex.Matches(inputLine)[0];
+                    groups = match.Groups;
+                    Logger.Debug($"Siren regex matched for input {inputLine}. Groups are {this.DumpGroups(groups)}");
+                    instanceId = groups["instance_id"].Value;
+                }
+                else
+                {
+                    // If we get here then the string can be from Faloop or Bear. Easiest way to discern them is to
+                    // check if the string contains '[S]', which is unique to Faloop
+                    if (inputLine.Contains("[S]"))
+                    {
+                        // We have a Faloop string
+                        match = faloopRegex.Matches(inputLine)[0];
+                        Logger.Debug($"Faloop regex matched for input {inputLine}. Groups are {this.DumpGroups(match.Groups)}");
+                    }
+                    else
+                    {
+                        // If a map on Bear doesn't have a mark's location then the coordinates are 'NOT AVAILABLE'
+                        if (inputLine.Contains("NOT AVAILABLE"))
+                        {
+                            Logger.Debug($"Input {inputLine} does not have coordinates. Ignoring");
+                            continue;
+                        }
+
+                        match = bearRegex.Matches(inputLine)[0];
+                        groups = match.Groups;
+                        Logger.Debug($"Bear regex matched for input {inputLine}. Groups are {this.DumpGroups(groups)}");
+                    }
+                    groups = match.Groups;
+                    // Faloop (and Bear) doesn't use the 1/2/3 instance symbols directly (while Siren does), so 
+                    // use this dictionary to get the symbol for the output
+                    instanceId = groups["instance_number"].Value.IsNullOrEmpty()
+                                     ? null
+                                     : instanceKeyMap[groups["instance_number"].Value];
+                }
+                var mapName = groups["map_name"].Value.Trim();
+                Maps.TryGetValue(mapName, out var map);
+                var markName = groups["mark_name"].Value;
+                var x = float.Parse(groups["x_coord"].Value, CultureInfo.InvariantCulture);
+                var y = float.Parse(groups["y_coord"].Value, CultureInfo.InvariantCulture);
+
+                marks.Add(new MarkInformation(map, x, y, instanceId, markName, inputLine, groups["map_name"].Value));
+            }
+            return marks;
+        }
+
+        public void EchoMarks(List<MarkInformation> marks)
+        {
+            foreach (var mark in marks)
+            {
+                SeString output;
+                if (mark.map != null)
+                {
+                    output = this.CreateMapLink(mark.map.TerritoryId, mark.map.RowId, mark.xCoord, mark.yCoord, mark.instanceId, mark.markName);
+                }
+                else
+                {
+                    var builder = new SeStringBuilder();
+                    builder.Append(
+                        $"Input text \"{mark.inputLine}\" invalid. Could not find a matching map for {mark.mapName}.");
+                    output = builder.Build();
+                }
+
+                this.Chat.Print(new XivChatEntry
+                {
+                    Type = XivChatType.Echo,
+                    Name = "",
+                    Message = output
+                });
+            }
+        }
+
+        // This is a custom version of Dalamud's CreateMapLink method. It includes the mark name and the instance ID
+        private SeString CreateMapLink(
+            uint territoryId,
+            uint mapId,
+            float xCoord,
+            float yCoord,
+            string? instanceId,
+            string markName)
+        {
+            string text;
+            MapLinkPayload mapLinkPayload = new MapLinkPayload(territoryId, mapId, xCoord, yCoord, 0.05f);
+            if (instanceId == null)
+            {
+                text = mapLinkPayload.PlaceName + " " + mapLinkPayload.CoordinateString;
+            }
+            else
+            {
+                text = mapLinkPayload.PlaceName + instanceId + " " + mapLinkPayload.CoordinateString;
+            }
+
+            List<Payload> payloads = new List<Payload>((IEnumerable<Payload>)new Payload[4]
+            {
+                (Payload)mapLinkPayload,
+                (Payload)new TextPayload(text),
+                (Payload)new TextPayload($" ({markName})"),
+                (Payload)RawPayload.LinkTerminator
+            });
+            payloads.InsertRange(1, SeString.TextArrowPayloads);
+            return new SeString(payloads);
+        }
+
+        private String DumpGroups(GroupCollection groups)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Group group in groups)
+            {
+                sb.Append($"({group.Name}:{group.Value}),");
+            }
+            sb.Remove(sb.Length - 1, 1);
+            return sb.ToString();
+        }
+    }
+}
--- CoordImporter/Plugin.cs
+++ CoordImporter/Plugin.cs
@@ -22,6 +22,18 @@ using Dalamud;
 
 namespace CoordImporter
 {
+    public class MapData
+    {
+        public uint TerritoryId { get; }
+        public uint RowId { get; }
+
+        public MapData(uint territoryId, uint rowId)
+        {
+            this.TerritoryId = territoryId;
+            this.RowId = rowId;
+        }
+    }
+
     public sealed class Plugin : IDalamudPlugin
     {
         public string Name => "Coordinate Importer";
@@ -33,7 +45,8 @@ namespace CoordImporter
         private IChatGui Chat { get; }
         private IDataManager DataManager { get; }
         private IPluginLog Logger { get; }
-        private IDictionary<String, Map> maps { get; set; }
+        private IDictionary<String, MapData> maps { get; set; }
+        private Importer Importer { get; init; }
 
         private MainWindow MainWindow { get; init; }
 
@@ -52,7 +65,7 @@ namespace CoordImporter
 
             // This should support names of maps in all available languages
             // We create a hashtable where the key is the name of the map (in whatever language) and the value is the map object
-            maps = new Dictionary<String, Map>();
+            maps = new Dictionary<String, MapData>();
             foreach (ClientLanguage cl in ClientLanguage.GetValuesAsUnderlyingType<ClientLanguage>())
             {
                 if (DataManager.GetExcelSheet<Map>(cl) != null)
@@ -69,7 +82,7 @@ namespace CoordImporter
                                 if (placeName != null)
                                 {
                                     Logger.Verbose($"Adding map with name {placeName.Name} with language {cl}");
-                                    if (!maps.TryAdd(placeName.Name, map))
+                                    if (!maps.TryAdd(placeName.Name, new MapData(map.TerritoryType.Value!.RowId, map.RowId)))
                                     {
                                         Logger.Verbose($"Attempted to add map with name {placeName.Name} for language {cl} but it already existed");
                                     }
@@ -80,6 +93,8 @@ namespace CoordImporter
                 }
                 Logger.Debug($"Loaded Map data from ClientLanguage {cl}");
             }
+
+            this.Importer = new Importer(Chat, Logger, maps);
 
             MainWindow = new MainWindow(this);
 
@@ -111,144 +126,9 @@ namespace CoordImporter
             this.WindowSystem.Draw();
         }
 
-        public void EchoString(string pastedPayload)
+        public void ParseAndEcho(String pastedBuffer)
         {
-            var instanceKeyMap = new Dictionary<String, String>()
-            {
-                { "1", "\ue0b1" },
-                { "2", "\ue0b2" },
-                { "3", "\ue0b3" },
-            };
-
-            var type = XivChatType.Echo;
-            var splitStrings = pastedPayload.Split(
-                new string[] { "\r\n", "\r", "\n" },
-                StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-            // For the format "(Maybe: Storsie) \ue0bbLabyrinthos ( 17  , 9.6 ) " (including the icky unicode instance/arrow)
-            var sirenRegex = new Regex(
-                @"(\(Maybe: (?<mark_name>[\w+ '-]+)\) \ue0bb)?(?<map_name>[\w+ '-]+)(?<instance_id>[\ue0b1\ue0b2\ue0b3]?)\s+\(\s*(?<x_coord>[0-9\.]+)\s*,\s*(?<y_coord>[0-9\.]+)\s*\)",
-                RegexOptions.Compiled);
-            // For the format "Labyrinthos ( 16.5 , 16.8 ) Storsie"
-            var bearRegex = new Regex(
-                @"(?<map_name>[\D'\s+-]*)\s*(?<instance_number>[123]?)\s*\(\s*(?<x_coord>[0-9\.]+)\s*,\s*(?<y_coord>[0-9\.]+)\s*\)\s*(?<mark_name>[\w+ +-]+)",
-                RegexOptions.Compiled);
-            // For the format "Raiden [S]: Gamma - Yanxia ( 23.6, 11.4 )"
-            var faloopRegex = new Regex(
-                @"(?<world_name>[a-zA-Z0-9'-]+)\s+\[S\]: (?<mark_name>[\w+ '-]+) - (?<map_name>[\w+ '-]+)\s+\(?(?<instance_number>[1-3]?)\)?\s*\(\s*(?<x_coord>[0-9\.]+)\s*,\s*(?<y_coord>[0-9\.]+)\s*\)",
-                RegexOptions.Compiled);
-
-            foreach (var inputLine in splitStrings)
-            {
-                SeString output;
-                Match match;
-                string? instanceId;
-                GroupCollection groups;
-                // Check if the little arrow symbol is in the text. If it is then the line is from Siren
-                if (inputLine.Contains("\ue0bb"))
-                {
-                    match = sirenRegex.Matches(inputLine)[0];
-                    groups = match.Groups;
-                    Logger.Debug($"Siren regex matched for input {inputLine}. Groups are {this.DumpGroups(groups)}");
-                    instanceId = groups["instance_id"].Value;
-                }
-                else
-                {
-                    // If we get here then the string can be from Faloop or Bear. Easiest way to discern them is to
-                    // check if the string contains '[S]', which is unique to Faloop
-                    if (inputLine.Contains("[S]"))
-                    {
-                        // We have a Faloop string
-                        match = faloopRegex.Matches(inputLine)[0];
-                        Logger.Debug($"Faloop regex matched for input {inputLine}. Groups are {this.DumpGroups(match.Groups)}");
-                    }
-                    else
-                    {
-                        // If a map on Bear doesn't have a mark's location then the coordinates are 'NOT AVAILABLE'
-                        if (inputLine.Contains("NOT AVAILABLE"))
-                        {
-                            Logger.Debug($"Input {inputLine} does not have coordinates. Ignoring");
-                            continue;
-                        }
-
-                        match = bearRegex.Matches(inputLine)[0];
-                        groups = match.Groups;
-                        Logger.Debug($"Bear regex matched for input {inputLine}. Groups are {this.DumpGroups(groups)}");
-                    }
-                    groups = match.Groups;
-                    // Faloop (and Bear) doesn't use the 1/2/3 instance symbols directly (while Siren does), so 
-                    // use this dictionary to get the symbol for the output
-                    instanceId = groups["instance_number"].Value.IsNullOrEmpty()
-                                     ? null
-                                     : instanceKeyMap[groups["instance_number"].Value];
-                }
-                Map? map = null;
-                var mapName = groups["map_name"].Value.Trim();
-                maps.TryGetValue(mapName, out map);
-                var markName = groups["mark_name"].Value;
-                var x = float.Parse(groups["x_coord"].Value, CultureInfo.InvariantCulture);
-                var y = float.Parse(groups["y_coord"].Value, CultureInfo.InvariantCulture);
-
-                if (map != null)
-                {
-                    output = this.CreateMapLink(map.TerritoryType.Value!.RowId, map.RowId, x, y, instanceId, markName);
-                }
-                else
-                {
-                    var builder = new SeStringBuilder();
-                    builder.Append(
-                        $"Input text \"{inputLine}\" invalid. Could not find a matching map for {groups["map_name"]}.");
-                    output = builder.Build();
-                }
-
-                this.Chat.Print(new XivChatEntry
-                {
-                    Type = type,
-                    Name = "",
-                    Message = output
-                });
-            }
-        }
-
-        // This is a custom version of Dalamud's CreateMapLink method. It includes the mark name and the instance ID
-        private SeString CreateMapLink(
-            uint territoryId,
-            uint mapId,
-            float xCoord,
-            float yCoord,
-            string? instanceId,
-            string markName)
-        {
-            string text;
-            MapLinkPayload mapLinkPayload = new MapLinkPayload(territoryId, mapId, xCoord, yCoord, 0.05f);
-            if (instanceId == null)
-            {
-                text = mapLinkPayload.PlaceName + " " + mapLinkPayload.CoordinateString;
-            }
-            else
-            {
-                text = mapLinkPayload.PlaceName + instanceId + " " + mapLinkPayload.CoordinateString;
-            }
-
-            List<Payload> payloads = new List<Payload>((IEnumerable<Payload>)new Payload[4]
-            {
-                (Payload)mapLinkPayload,
-                (Payload)new TextPayload(text),
-                (Payload)new TextPayload($" ({markName})"),
-                (Payload)RawPayload.LinkTerminator
-            });
-            payloads.InsertRange(1, SeString.TextArrowPayloads);
-            return new SeString(payloads);
-        }
-
-        private String DumpGroups(GroupCollection groups)
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach (Group group in groups)
-            {
-                sb.Append($"({group.Name}:{group.Value}),");
-            }
-            sb.Remove(sb.Length - 1, 1);
-            return sb.ToString();
+            Importer.EchoMarks(Importer.ParsePayload(pastedBuffer));
         }
     }
 }
