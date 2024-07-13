3693f6dc8a8c6eac3657c4cd119c3828ced1f9b3
ditzy <ditzy.zy@pm.me>
2024-07-13 17:53:42 -0500

    parameterize the tests and make bear stop thinking it can parse turtle

--- CoordImporter/Parsers/BearParser.cs
+++ CoordImporter/Parsers/BearParser.cs
@@ -3,12 +3,15 @@ using System.Text.RegularExpressions;
 using CoordImporter.Managers;
 using CoordImporter.Models;
 using CSharpFunctionalExtensions;
+using Dalamud.Game.Text;
 using Dalamud.Plugin.Services;
 
 namespace CoordImporter.Parsers;
 
 public class BearParser : Parser
 {
+    private static readonly char LinkChar = SeIconChar.LinkMarker.ToIconChar();
+    
     // For the format "Labyrinthos ( 16.5 , 16.8 ) Storsie"
     protected readonly Regex Regex = new Regex(
         @"(?<map_name>[\D'\s-]+)\s*(?<instance>[1-9]?)\s*\(\s*((?<x_coord>[0-9\.]+)\s*,\s*(?<y_coord>[0-9\.]+)|NOT AVAILABLE)\s*\)\s*(?<mark_name>\w[\w '-]+)",
@@ -22,7 +25,7 @@ public class BearParser : Parser
         return CreateMark(groups, instance => (uint)(instance.First() - '0'));
     }
 
-    public override bool CanParseLine(string inputLine) => Regex.IsMatch(inputLine);
+    public override bool CanParseLine(string inputLine) => !inputLine.Contains(LinkChar) && Regex.IsMatch(inputLine);
 
     public BearParser(IPluginLog logger, IDataManagerManager dataManagerManager) : base(logger, dataManagerManager) { }
 }
